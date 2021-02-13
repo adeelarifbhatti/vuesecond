5669ee22e4dbc7c1b64d76ae8c4cd9cfb5640353
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-02-13 13:52:35 +0100

    Inprogress. Lifecycle Hooks is going to be next

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1> Hello World</h1>
+<button @click="start" :disabled="isPlaying"> Start </button> 
+<Attempt v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import  Attempt from './components/Attempt.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:
+
+   { Attempt },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+
+  },
+  methods: {
+      start (){
+        this.delay = 2000 + Math.random() * 50000
+        this.isPlaying = true
+        console.log(this.delay);
+      }
   }
 }
 </script>
@@ -20,7 +35,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Attempt.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="block"> </div>
+  
+</template>
+
+<script>
+props: ['delays']
+
+export default {
+
+}
+</script>
+
+<style>
+.block {
+    width: 700px;
+    border-radius: 20px;
+    background: green;
+    padding: 100px 0;
+    margin: 40px auto;
+
+}
+
+</style>
