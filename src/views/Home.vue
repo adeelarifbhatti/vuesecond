c951eb937baec7dd4c9cf1fa5fd014579edb1152
root <root@mol.uio.no>
2021-02-16 12:07:25 +0100

    New Vue with Router

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+<p class="paragraph"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tempora iste error dicta veniam explicabo praesentium perferendis quibusdam assumenda libero. Officia, vero deleniti odit molestiae voluptatibus nesciunt, accusamus incidunt voluptas placeat qui quam excepturi? Ipsam repellat consequatur saepe molestias sit illo perspiciatis? Dolorum in rem voluptatem, officia doloribus itaque, eum totam quos, cum deserunt neque provident ex blanditiis quisquam! Deleniti eveniet accusantium optio reiciendis, neque quis totam autem nulla quisquam ab animi facilis saepe alias eaque tempore rerum! Quia velit fugiat reprehenderit quam minima, soluta illum ex id, hic perspiciatis voluptatum, dolores et quae repudiandae dolorum assumenda voluptatem placeat laborum.</p>
+ <p class="paragraph"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tempora iste error dicta veniam explicabo praesentium perferendis quibusdam assumenda libero. Officia, vero deleniti odit molestiae voluptatibus nesciunt, accusamus incidunt voluptas placeat qui quam excepturi? Ipsam repellat consequatur saepe molestias sit illo perspiciatis? Dolorum in rem voluptatem, officia doloribus itaque, eum totam quos, cum deserunt neque provident ex blanditiis quisquam! Deleniti eveniet accusantium optio reiciendis, neque quis totam autem nulla quisquam ab animi facilis saepe alias eaque tempore rerum! Quia velit fugiat reprehenderit quam minima, soluta illum ex id, hic perspiciatis voluptatum, dolores et quae repudiandae dolorum assumenda voluptatem placeat laborum.</p>
+<p class="paragraph">  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tempora iste error dicta veniam explicabo praesentium perferendis quibusdam assumenda libero. Officia, vero deleniti odit molestiae voluptatibus nesciunt, accusamus incidunt voluptas placeat qui quam excepturi? Ipsam repellat consequatur saepe molestias sit illo perspiciatis? Dolorum in rem voluptatem, officia doloribus itaque, eum totam quos, cum deserunt neque provident ex blanditiis quisquam! Deleniti eveniet accusantium optio reiciendis, neque quis totam autem nulla quisquam ab animi facilis saepe alias eaque tempore rerum! Quia velit fugiat reprehenderit quam minima, soluta illum ex id, hic perspiciatis voluptatum, dolores et quae repudiandae dolorum assumenda voluptatem placeat laborum.</p>
+ 
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+<style>
+.paragraph {
+  text-align: justify;
+  text-justify: inter-word;
+  
+}
+</style>
